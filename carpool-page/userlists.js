702cb43b557487720d60fde096d88911fdeabbf2
Curtis Lewis <culewis@pdx.edu>
2019-12-02 17:55:45 -0800

    Demo names and emails are populated now. Just need to get Google location for each to show on map

--- carpool-page/userlists.js
+++ carpool-page/userlists.js
@@ -4,9 +4,32 @@ function populate_drivers() {
   let counter = 1;
   for (x in driverObj) {
     id = "driver";
-    id.concat(counter.toString(10));
-    document.getElementById(id).innerHTML = x;
+    id = id.concat(counter.toString(10));
+    document.getElementById(id).textContent = x;
+    //update email link
+    id = id.concat("-mail");
+    let mail = "mailto:".concat(driverObj[x].email);
+    document.getElementById(id).setAttribute("href", mail);
+    counter += 1;
   }
 }
 
+function populate_riders() {
+  let riderObj = Riders;
+  let x, id;
+  let counter = 1;
+  for (x in riderObj) {
+    id = "rider";
+    id = id.concat(counter.toString(10));
+    //update name
+    document.getElementById(id).textContent = x;
+    //update email link
+    id = id.concat("-mail");
+    let mail = "mailto:".concat(riderObj[x].email);
+    document.getElementById(id).setAttribute("href", mail);
+    counter += 1;
+  }
+}
 
+populate_drivers();
+populate_riders();
